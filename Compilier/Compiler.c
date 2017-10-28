4a44cdaba6ea14b80e21efb0497815a7259478e4
BEANefiT <yunusov.rr@phystech.edu>
2017-10-28 18:29:07 +0300

    trouble with an infinite cycle

--- Compilier/Compiler.c
+++ Compilier/Compiler.c
@@ -50,111 +50,36 @@ size_t srcSize( FILE *src )
 
 int compile( char *src, size_t src_sz, size_t *exe_sz, enum Compilier_modes mode, size_t *labels, void *exe )
 {
-	#define DEF_CMD( name, num ) const int CMD_##name = num;
+	#define DEF_CMD( NAME,  name, num, Cmplr_code2, Cmplr_code1, CPU_code ) const int CMD_##NAME = num;
+
 	#include "commands.h"
+
 	#undef DEF_CMD
 
 	void *exe_cur = exe;
 	int src_cur = 0;
 	int src_cur_delta = 0;
 
-	#define _Compile_
-	#include "commands.h"
-
 	if( mode == first )
 	{
 		while( src_cur < src_sz )
 		{
 			char *str = ( char * )calloc( 10, sizeof( char ) );
-			sscanf( src + src_cur, "%s%n", str, &src_cur_delta );
-			src_cur += src_cur_delta;
-			if( strcmp( str, "label" ) == 0 )
-			{
-				CMD_LABEL_code;
-			}
-			if( strcmp( str, "push" ) == 0 )
-			{
-				char *tmp = ( char * )calloc( 8, sizeof( char ) );
-				char tmpc = 0;
-				sscanf( src + src_cur + 1, "%c", &tmpc );
-				if( tmpc == '[' )
-				{
-					exe_cur += 2 * sizeof( int );
-				}
-				else
-				{
-					sscanf( src + src_cur, "%s", tmp );
-					if( strcmp( tmp, "ax" ) == 0 || strcmp( tmp, "bx" ) == 0 || strcmp( tmp, "cx" ) == 0 || strcmp( tmp, "dx" ) == 0 )
-					{
-						exe_cur += 2 * sizeof( int );
-					}
-					else
-					{
-						exe_cur += ( sizeof( int ) + sizeof( double ) );
-					}
-				}
-				free( tmp );
-			}
-			if( strcmp( str, "pop" ) == 0 )
-			{
-				exe_cur += 2 * sizeof( int );
-			}
-			if( strcmp( str, "add" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "mul" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "div" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "sub" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "out" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "jmp" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "je" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "jne" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "ja" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "jae" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "jb" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "jbe" ) == 0 )
-			{
-				exe_cur += sizeof( int ) + sizeof( size_t );
-			}
-			if( strcmp( str, "sqrt" ) == 0 )
-			{
-				exe_cur += sizeof( int );
-			}
+			from_src( %s, str );
+			printf( "%d\n", src_cur );
+			#define DEF_CMD( NAME, name, num, Cmplr_code2, Cmplr_code1, CPU_code )		\
+			do										\
+			{										\
+				if( strcmp( str, #name ) == 0 )						\
+					exe_cur += Cmplr_code1;						\
+			}while( 0 )
+
+			#include "commands.h"
+
+			#undef DEF_CMD
 
 			free( str );
 		}
-
 		return 0;
 	}
 	
@@ -163,176 +88,19 @@ int compile( char *src, size_t src_sz, size_t *exe_sz, enum Compilier_modes mode
 		while( src_cur < src_sz )							
 		{
 			char *str = ( char * )calloc( 10, sizeof( char ) );
-			sscanf( src + src_cur, "%s%n", str, &src_cur_delta );
-			src_cur += src_cur_delta;
-	
-			if( strcmp( str, "push" ) == 0 )
-			{
-				char *tmp = ( char * )calloc( 8, sizeof( char ) );
-				char tmpc = 0;
-				sscanf( src + src_cur + 1, "%c", &tmpc );
-				if( tmpc == '[' )
-				{
-					sscanf( src + src_cur + 1, "[%[^]]]%n", tmp, &src_cur_delta );
-					if( strcmp( tmp, "ax" ) == 0 || strcmp( tmp, "bx" ) == 0 || strcmp( tmp, "cx" ) == 0 || strcmp( tmp, "dx" ) == 0 )
-					{
-						src_cur += src_cur_delta;
-						memcpy( exe_cur, &CMD_RAMPUSHR, sizeof( int ) );
-						exe_cur += sizeof( int );
-						int reg_num = 0;
-						REG_READ_code();
-					}
-					else
-					{
-						int index = 0;
-						sscanf( src + src_cur, "[%d]%n", &index, src_cur_delta );
-						src_cur += src_cur_delta;
-						memcpy( exe_cur, &CMD_RAMPUSH, sizeof( int ) );
-						exe_cur += sizeof( int );
-						memcpy( exe_cur, &index, sizeof( int ) );
-						exe_cur += sizeof( int );
-					}
-				}
-				else
-				{
-					sscanf( src + src_cur, "%s%n", tmp, &src_cur_delta );
-					if( strcmp( tmp, "ax" ) == 0 || strcmp( tmp, "bx" ) == 0 || strcmp( tmp, "cx" ) == 0 || strcmp( tmp, "dx" ) == 0 )
-					{
-						src_cur += src_cur_delta;
-						memcpy( exe_cur, &CMD_PUSHR, sizeof( int ) );
-						exe_cur += sizeof( int );
-						int reg_num = 0;
-						REG_READ_code();
-					}
-					else
-					{
-						memcpy( exe_cur, &CMD_PUSH, sizeof( int ) );
-						exe_cur += sizeof( int );
-						CMD_PUSH_code;
-					}
-					free( tmp );
-				}
-			}
-			if( strcmp( str, "pop" ) == 0 )
-			{
-				char *tmp = ( char * )calloc( 8, sizeof( char ) );
-				char tmpc = 0;
-				sscanf( src + src_cur + 1, "%c", &tmpc );
-				if( tmpc == '[' )
-				{
-					sscanf( src + src_cur + 1, "[%[^]]]n", tmp, &src_cur_delta );
-					if( strcmp( tmp, "ax" ) == 0 || strcmp( tmp, "bx" ) == 0 || strcmp( tmp, "cx" ) == 0 || strcmp( tmp, "dx" ) == 0 )
-					{
-						src_cur += src_cur_delta;
-						memcpy( exe_cur, &CMD_RAMPOPR, sizeof( int ) );
-						exe_cur += sizeof( int );
-						int reg_num = 0;
-						REG_READ_code();
-					}
-					else
-					{
-						int index = 0;
-						sscanf( src + src_cur, "[%d]%n", &index, src_cur_delta );
-						src_cur += src_cur_delta;
-						memcpy( exe_cur, &CMD_RAMPOP, sizeof( int ) );
-						exe_cur += sizeof( int );
-						memcpy( exe_cur, &index, sizeof( int ) );
-						exe_cur += sizeof( int );
-					}
-				}
-				else
-				{
-					sscanf( src + src_cur, "%s%n", tmp, &src_cur_delta );
-					src_cur += src_cur_delta;
-					memcpy( exe_cur, &CMD_POPR, sizeof( int ) );
-					exe_cur += sizeof( int );
-					int reg_num = 0;
-					REG_READ_code();
-				}
-			}
-			if( strcmp( str, "add" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_ADD, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "mul" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_MUL, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "div" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_DIV, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "sub" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_SUB, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "out" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_OUT, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "jmp" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JMP, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "je" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JE, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "jne" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JNE, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "ja" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JA, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "jae" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JAE, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "jb" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JB, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "jbe" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_JBE, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "call" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_CALL, sizeof( int ) );
-				exe_cur += sizeof( int );
-				CMD_JMP_code;
-			}
-			if( strcmp( str, "ret" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_RET, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
-			if( strcmp( str, "sqrt" ) == 0 )
-			{
-				memcpy( exe_cur, &CMD_SQRT, sizeof( int ) );
-				exe_cur += sizeof( int );
-			}
+			from_src( %s, str );
+			//printf( "%d ", src_cur );
+			#define DEF_CMD( NAME, name, num, Cmplr_code2, Cmplr_code1,  CPU_code )	\
+			do										\
+			{										\
+				if( strcmp( str, #name ) == 0 )						\
+					Cmplr_code2;							\
+			}while( 0 )
+
+			#include "commands.h"
+
+			#undef DEF_CMD
+
 			free( str );
 		}
 		*exe_sz = ( size_t ) exe_cur - ( size_t ) exe;
--- Compilier/commands.h
+++ Compilier/commands.h
@@ -1,43 +1,203 @@
 #ifdef DEF_CMD
-DEF_CMD( PUSH,     1 )
-DEF_CMD( ADD,      2 )
-DEF_CMD( MUL,      3 )
-DEF_CMD( DIV,      4 )
-DEF_CMD( SUB,      5 )
-DEF_CMD( OUT,      6 )
-DEF_CMD( POPR,     7 )
-DEF_CMD( PUSHR,    8 )
-DEF_CMD( LABEL,    9 )
-DEF_CMD( JMP, 	   10 )
-DEF_CMD( JE,	   11 )
-DEF_CMD( JNE,	   12 )
-DEF_CMD( JA,	   13 )
-DEF_CMD( JAE,	   14 )
-DEF_CMD( JB,	   15 )
-DEF_CMD( JBE,	   16 )
-DEF_CMD( CALL,	   17 )
-DEF_CMD( RET,	   18 )
-DEF_CMD( RAMPUSH,  19 )
-DEF_CMD( RAMPUSHR, 20 )
-DEF_CMD( RAMPOP,   21 )
-DEF_CMD( RAMPOPR,  22 )
-DEF_CMD( SQRT,	   23 )
-#endif
+DEF_CMD( PUSH, push, 1, {
+				int push_type = 0;
+				void *arg = calloc( 1, sizeof( int ) );
+				push_type();
+				int cmd = CMD_PUSH + push_type;
+				to_exe( &cmd, int );
+				if( push_type != 0 )
+				{
+					to_exe( arg, int );
+				}
+				else
+				{
+					to_exe( arg, double );
+				}
+				free( arg );
+			},
 
-#ifdef _Compile_
-#define CMD_PUSH_code							\
+      2 * sizeof( int );{
+				int push_type = 0;
+				void *arg = calloc( 1, sizeof( int ) );
+				push_type();
+				free( arg );
+				if( push_type == 0 )
+					exe_cur += sizeof( int );
+      			},
+
+			{
+
+			});
+DEF_CMD( PUSHR, push, 2, , 0,  );
+DEF_CMD( RAMPUSH, push, 3, , 0,  );
+DEF_CMD( RAMPUSHR, push, 4, , 0,  );
+DEF_CMD( POPR, pop, 5, {
+				int push_type = 0;
+				void *arg = calloc( 1, sizeof( int ) );
+				push_type();
+				int cmd = CMD_POPR + push_type - 1;
+				to_exe( &cmd, int );
+				to_exe( arg, int );
+				free( arg );
+			},
+
+			2 * sizeof( int ),
+			
+			{
+
+			});
+DEF_CMD( RAMPOP, pop, 6, , 0, );
+DEF_CMD( RAMPOPR, pop, 7, , 0, );
+DEF_CMD( ADD, add, 8,  { to_exe( &CMD_ADD, int ); }, sizeof( int ),  );
+DEF_CMD( MUL, mul, 9,  { to_exe( &CMD_MUL, int ); }, sizeof( int ),  );
+DEF_CMD( DIV, div, 10, { to_exe( &CMD_DIV, int ); }, sizeof( int ),  );
+DEF_CMD( SUB, sub, 11, { to_exe( &CMD_SUB, int ); }, sizeof( int ),  );
+DEF_CMD( OUT, out, 12, { to_exe( &CMD_OUT, int ); }, sizeof( int ),  );
+DEF_CMD( LABEL, label, 13, ,  0;{
+					int label = -1;
+					from_src( %d, &label );
+					labels[ label ] = ( size_t )exe_cur - ( size_t )exe;
+				},
+
+				{
+
+				});
+DEF_CMD( JMP, jmp, 14,  {
+				to_exe( &CMD_JMP, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JE, je, 15,    {
+				to_exe( &CMD_JE, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JNE, jne, 16,  {
+				to_exe( &CMD_JNE, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JA, ja, 17,    {
+				to_exe( CMD_JA, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JAE, jae, 18,  {
+				to_exe( &CMD_JAE, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JB, jb, 19,    {
+				to_exe( &CMD_JB, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( JBE, jbe, 20,  {
+				to_exe( &CMD_JBE, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( CALL, call, 21,{
+				to_exe( &CMD_CALL, int );
+				CMD_JMP_code();
+			},
+
+			sizeof( int ) + sizeof( size_t ),
+			
+			{
+				
+			});
+DEF_CMD( RET, ret, 22, { to_exe( &CMD_RET, int ); }, sizeof( int ),  );
+DEF_CMD( SQRT, sqrt, 23, { to_exe( &CMD_SQRT, int ); }, sizeof( int ),  );
+#endif /*DEF_CMD*/
+
+#define push_type()							\
 do									\
 {									\
-	double value = 0;						\
-	sscanf( src + src_cur, "%lg%n", &value, &src_cur_delta );	\
-	src_cur += src_cur_delta;					\
-	memcpy( exe_cur, &value, sizeof( double ) );			\
-	exe_cur += sizeof( double );					\
+	char *tmp = ( char * )calloc( 8, sizeof( char ) );		\
+	char tmpc = 0;							\
+	check_src( %c, &tmpc );						\
+	while( tmpc == ' ' )						\
+	{								\
+		src_cur += 1;						\
+		check_src( %c, &tmpc );					\
+	}								\
+	if( tmpc == '[' )						\
+	{								\
+		src_cur += 1;						\
+		check_src( [%[^]]], tmp );				\
+		if( ifreg( tmp ) )					\
+		{							\
+			push_type = 3;					\
+			REG_READ_code();				\
+		}							\
+		else							\
+		{							\
+			push_type = 2;					\
+			int index;					\
+			from_src( %d, &index );				\
+			memcpy( arg, &index, sizeof( int ) );		\
+		}							\
+	}								\
+	else								\
+	{								\
+		check_src( %s, tmp );					\
+		if( ifreg( tmp ) )					\
+		{							\
+			push_type = 1;					\
+			REG_READ_code();				\
+		}							\
+		else							\
+		{							\
+			arg = realloc( arg, sizeof( double ) );		\
+			double value = 0;				\
+			from_src( %lg, &value );			\
+			memcpy( arg, &value, sizeof( double ) );	\
+		}							\
+	}								\
+	free( tmp );							\
 } while( 0 )
 
 #define REG_READ_code()							\
 do									\
 {									\
+	src_cur += src_cur_delta;					\
+	int reg_num = 0;						\
 	if( strcmp( tmp, "ax" ) == 0 )					\
 		reg_num = 1;						\
 	if( strcmp( tmp, "bx" ) == 0 )					\
@@ -46,29 +206,35 @@ do									\
 		reg_num = 3;						\
 	if( strcmp( tmp, "dx" ) == 0 )					\
 		reg_num = 4;						\
-									\
-	memcpy( exe_cur, &reg_num, sizeof( int ) );			\
-	exe_cur += sizeof( int );					\
+	memcpy( arg, &reg_num, sizeof( int ) );				\
 } while( 0 )
 
-#define CMD_LABEL_code							\
+#define CMD_JMP_code()							\
 do									\
 {									\
 	int label = -1;							\
-	sscanf( src + src_cur, "%d%n", &label, &src_cur_delta );	\
-	src_cur += src_cur_delta;					\
-	labels[ label ] = ( size_t )exe_cur - ( size_t )exe;		\
+	from_src( %d, &label );						\
+	to_exe( labels[ label ], size_t );				\
 } while( 0 )
 
+#define ifreg( tmp ) strcmp( tmp, "ax" ) == 0 || strcmp( tmp, "bx" ) == 0 || strcmp( tmp, "cx" ) == 0 || strcmp( tmp, "dx" ) == 0
 
-#define CMD_JMP_code							\
+#define	to_exe( ptr, size )						\
 do									\
 {									\
-	int label = -1;							\
-	sscanf( src + src_cur, "%d%n", &label, &src_cur_delta );	\
-	src_cur += src_cur_delta;					\
-	memcpy( exe_cur, labels + label, sizeof( size_t ) );		\
-	exe_cur += sizeof( size_t );					\
+	memcpy( exe_cur, ptr, sizeof( size ) );			\
+	exe_cur += sizeof( size );					\
 } while( 0 )
 
-#endif /*__Compile__*/
+#define from_src( type, dest )						\
+do									\
+{									\
+	sscanf( src + src_cur, "type%n", dest, &src_cur_delta );	\
+	src_cur += src_cur_delta;					\
+} while( 0 )
+
+#define check_src( type, dest )						\
+do									\
+{									\
+	sscanf( src + src_cur, "type%n", dest, &src_cur_delta );	\
+} while( 0 )
