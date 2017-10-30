c5ff8f413351a1e28acda1c51f0fa0475d1972d2
BEANefiT <yunusov.rr@phystech.edu>
2017-10-30 20:37:55 +0300

    Integrated header

--- Compilier/Compiler.c
+++ Compilier/Compiler.c
@@ -4,38 +4,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include "commands.h"
-
-enum Compilier_modes
-{
-	first,
-	second
-};
-
-struct Compilier_structure
-{
-	char *src;
-	size_t src_sz;
-	size_t *labels;
-	void *exe;
-	size_t exe_sz;
-	enum Compilier_modes mode;
-};
-
-int Compilier_constr( struct Compilier_structure *Compilier );
-char *get_src( char *src_file_name, struct Compilier_structure *Compilier );
-size_t srcSize( FILE *src );
-int compile( struct Compilier_structure *Compilier );
-int makecode( struct Compilier_structure *Compilier );
+#include "Compiler.h"
 
 int main( int argc, char *argv[] )
 {
 	struct Compilier_structure Compilier;
 	Compilier_constr( &Compilier );
 	Compilier.src = get_src( argv[ 1 ], &Compilier );
+
 	Compilier.mode = first;
 	compile( &Compilier );
 	Compilier.mode = second;
 	compile( &Compilier );
+
 	makecode( &Compilier );
 
 	return 0;
