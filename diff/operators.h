9b04062cf62da0ecd7d4eacdeb73ad9e1a390add
BEANefiT <yunusov.rr@phystech.edu>
2017-12-06 01:59:32 +0300

    fixed pow bugs

--- diff/operators.h
+++ diff/operators.h
@@ -7,7 +7,7 @@ OPER_DEF( div_cmd, bin, DIV, /,   DIV( tree, SUB( tree, MUL( tree, dL, R ), MUL(
 OPER_DEF( ln_cmd,  uno, LN,  ln,  DIV( tree, dL, L ) )
 OPER_DEF( sin_cmd, uno, SIN, sin, MUL( tree, dL, COS( tree, L ) ) )
 OPER_DEF( cos_cmd, uno, COS, cos, MUL( tree, num( tree, -1 ), MUL( tree, dL, SIN( tree, L ) ) ) )
-OPER_DEF( pow_cmd, bin, POW, ^,   MUL( tree, R, POW( tree, L, SUB( tree, R, num( tree, 1 ) ) ) ) )
+OPER_DEF( pow_cmd, bin, POW, ^,   diff( tree, EXP( tree, MUL( tree, R, LN( tree, L ) ) ), dvar ) )
 OPER_DEF( tan_cmd, uno, TAN, tan, DIV( tree, dL, POW( tree, COS( tree, L ), num( tree, 2 ) ) ) )
 OPER_DEF( cot_cmd, uno, COT, cot, MUL( tree, num( tree, -1 ), DIV( tree, dL, POW( tree, SIN( tree, L ), num( tree, 2 ) ) ) ) )
 OPER_DEF( exp_cmd, uno, EXP, exp, MUL( tree, dL, EXP( tree, L ) ) )
